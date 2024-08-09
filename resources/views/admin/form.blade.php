96b604ea16b7574b6ad9cc4671267698d63ebfcf
sarkarmamun171 <sarkar.pau.cse171@gmail.com>
2024-08-09 11:57:59 +0600

    Managment system form added

--- resources/views/admin/form.blade.php
+++ resources/views/admin/form.blade.php
@@ -11,12 +11,11 @@
             href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
 
         <link rel="stylesheet"
-            href="../../plugins/fontawesome-free/css/all.min.css">
+            href="{{ asset('admin') }}/plugins/fontawesome-free/css/all.min.css">
 
         <link rel="stylesheet"
-            href="../../dist/css/adminlte.min2167.css?v=3.2.0">
-        <script
-            nonce="490c5839-b944-4b69-9321-1f9cbff289ee">try{(function(w,d){!function(j,k,l,m){j[l]=j[l]||{};j[l].executed=[];j.zaraz={deferred:[],listeners:[]};j.zaraz._v="5621";j.zaraz.q=[];j.zaraz._f=function(n){return async function(){var o=Array.prototype.slice.call(arguments);j.zaraz.q.push({m:n,a:o})}};for(const p of["track","set","debug"])j.zaraz[p]=j.zaraz._f(p);j.zaraz.init=()=>{var q=k.getElementsByTagName(m)[0],r=k.createElement(m),s=k.getElementsByTagName("title")[0];s&&(j[l].t=k.getElementsByTagName("title")[0].text);j[l].x=Math.random();j[l].w=j.screen.width;j[l].h=j.screen.height;j[l].j=j.innerHeight;j[l].e=j.innerWidth;j[l].l=j.location.href;j[l].r=k.referrer;j[l].k=j.screen.colorDepth;j[l].n=k.characterSet;j[l].o=(new Date).getTimezoneOffset();if(j.dataLayer)for(const w of Object.entries(Object.entries(dataLayer).reduce(((x,y)=>({...x[1],...y[1]})),{})))zaraz.set(w[0],w[1],{scope:"page"});j[l].q=[];for(;j.zaraz.q.length;){const z=j.zaraz.q.shift();j[l].q.push(z)}r.defer=!0;for(const A of[localStorage,sessionStorage])Object.keys(A||{}).filter((C=>C.startsWith("_zaraz_"))).forEach((B=>{try{j[l]["z_"+B.slice(7)]=JSON.parse(A.getItem(B))}catch{j[l]["z_"+B.slice(7)]=A.getItem(B)}}));r.referrerPolicy="origin";r.src="../../../../cdn-cgi/zaraz/sd0d9.js?z="+btoa(encodeURIComponent(JSON.stringify(j[l])));q.parentNode.insertBefore(r,q)};["complete","interactive"].includes(k.readyState)?zaraz.init():j.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document)}catch(e){throw fetch("/cdn-cgi/zaraz/t"),e;};</script></head>
+            href="{{ asset('admin') }}/dist/css/adminlte.min2167.css?v=3.2.0">
+       </head>
     <body class="hold-transition sidebar-mini">
         <div class="wrapper">
 
@@ -2053,17 +2052,17 @@
 
         </div>
 
-        <script src="../../plugins/jquery/jquery.min.js"></script>
+        <script src="{{ asset('admin') }}/plugins/jquery/jquery.min.js"></script>
 
         <script
-            src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+            src="{{ asset('admin') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 
         <script
-            src="../../plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+            src="{{ asset('admin') }}/plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
 
-        <script src="../../dist/js/adminlte.min2167.js?v=3.2.0"></script>
+        <script src="{{ asset('admin') }}/dist/js/adminlte.min2167.js?v=3.2.0"></script>
 
-        <script src="../../dist/js/demo.js"></script>
+        <script src="{{ asset('admin') }}/dist/js/demo.js"></script>
 
         <script>
 $(function () {
